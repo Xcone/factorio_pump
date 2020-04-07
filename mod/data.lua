c987966cd73ac5c987568eef506e4819d75c15eb
Koen Visser <koen@xcone.net>
2020-04-07 23:08:09 +0200

    Set selection-tool to only select oil wells

--- mod/data.lua
+++ mod/data.lua
@@ -9,10 +9,21 @@ pumpSelectionTool.icons= {
       tint={r=1,g=0.5,b=1,a=1}
    },
 }
-pumpSelectionTool.selection_mode = {"trees"}
-pumpSelectionTool.alt_selection_mode = {"trees"}
+pumpSelectionTool.selection_mode = {"any-entity"}
 pumpSelectionTool.selection_cursor_box_type = "entity"
 pumpSelectionTool.flags = {"only-in-cursor"}
+pumpSelectionTool.entity_filters = {"crude-oil"}
+
+-- no different ALT-behavior. Just copy it from the regular behavior
+pumpSelectionTool.alt_selection_color = pumpSelectionTool.selection_color
+pumpSelectionTool.alt_selection_cursor_box_type = pumpSelectionTool.selection_cursor_box_type
+pumpSelectionTool.alt_selection_mode = pumpSelectionTool.selection_mode
+pumpSelectionTool.alt_entity_filter_mode = pumpSelectionTool.entity_filter_mode
+pumpSelectionTool.alt_entity_filters = pumpSelectionTool.entity_filters
+pumpSelectionTool.alt_entity_type_filters = pumpSelectionTool.entity_type_filters
+pumpSelectionTool.alt_tile_filter_mode = pumpSelectionTool.tile_filter_mode
+pumpSelectionTool.alt_tile_filters = pumpSelectionTool.tile_filters
+
 
 local pumpShortcut = table.deepcopy(data.raw["shortcut"]["give-blueprint"])
 pumpShortcut.name = "pump-shortcut"
