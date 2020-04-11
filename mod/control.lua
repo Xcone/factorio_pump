6a7278a2da51b36b3ffca06fe2bf3b9d5aa5c2a4
Koen Visser <koen@xcone.net>
2020-04-11 23:11:43 +0200

    Gather info about the area in preparation for a separate planner-script

--- mod/control.lua
+++ mod/control.lua
@@ -1,30 +1,62 @@
 -- control.lua
 script.on_event({defines.events.on_player_selected_area}, function(event)
     if event.item == 'pump-selection-tool' then
-        local player = game.get_player(event.player_index)
-
-        if #event.entities == 0 then
-            player.print(
-                "P.U.M.P. cannot place pumpjacks, because there are no oil wells found in selected area.")
-            return
-        end
-
-        if pipes_present_in_area(event.surface, event.area) then
-            player.print(
-                "P.U.M.P. cannot safely place pumpjacks and pipes, because there are other pipes present within the selected area.")
-            return
-        end
-
-        local count = 0;
-        for i, entity in ipairs(event.entities) do
-
-            local direction = defines.direction.east
-            if can_place_pumpjack(event.surface, entity.position, direction) then
-                place_pumpjack(event.surface, entity.position, direction)
-            end
-        end
+        process_selected_area_with_this_mod(event)
     end
 end)
+
+function process_selected_area_with_this_mod(event)
+    local player = game.get_player(event.player_index)
+
+    if #event.entities == 0 then
+        player.print(
+            "P.U.M.P. cannot place pumpjacks, because there are no oil wells found in selected area.")
+        return
+    end
+
+    trim_event_area(event)
+
+    if pipes_present_in_area(event.surface, event.area) then
+        player.print(
+            "P.U.M.P. cannot safely place pumpjacks and pipes, because there are other pipes present within the selected area.")
+        return
+    end
+
+    local planner_input = prepare_planner_input(event)
+
+    for i, entity in ipairs(event.entities) do
+
+        local direction = defines.direction.east
+        if can_place_pumpjack(event.surface, entity.position, direction) then
+            place_pumpjack(event.surface, entity.position, direction)
+        end
+    end
+end
+
+function trim_event_area(event)
+    local uninitialized = true
+    local padding = 2 -- 1 for the size of the pump, 1 more for outgoing pipe
+
+    for i, entity in ipairs(event.entities) do
+        if entity.position.x < event.area.left_top.x or uninitialized then
+            event.area.left_top.x = entity.position.x - padding
+        end
+
+        if entity.position.y < event.area.left_top.y or uninitialized then
+            event.area.left_top.y = entity.position.y - padding
+        end
+
+        if entity.position.x > event.area.right_bottom.x or uninitialized then
+            event.area.right_bottom.x = entity.position.x + padding
+        end
+
+        if entity.position.y > event.area.right_bottom.y or uninitialized then
+            event.area.right_bottom.y = entity.position.y + padding
+        end
+
+        uninitialized = false
+    end
+end
 
 function pipes_present_in_area(surface, area)
     -- make search area one larger. This will make sure pipes right outside the selection are also found
@@ -50,11 +82,85 @@ function pipes_present_in_area(surface, area)
     return #found_pipes > 0 or #found_ghosts_of_pipes > 0
 end
 
+function prepare_planner_input(event)
+    local planner_input = {area = {}}
+
+    -- fill the map with default data 
+    for x = event.area.left_top.x, event.area.right_bottom.x, 1 do
+        planner_input.area[x] = {}
+        for y = event.area.left_top.y, event.area.right_bottom.y, 1 do
+            planner_input.area[x][y] = "undefined"
+        end
+    end
+
+    dump_planner_input(planner_input, "undefined-only")
+
+    -- mark where the pumps will be
+    for i, entity in ipairs(event.entities) do
+        local direction = defines.direction.east
+        planner_input.area[entity.position.x][entity.position.y] = "oil-well"
+        if can_place_pumpjack(event.surface, entity.position, direction) then
+            planner_input.area[entity.position.x - 1][entity.position.y - 1] =
+                "reserved-for-pump"
+            planner_input.area[entity.position.x - 1][entity.position.y] =
+                "reserved-for-pump"
+            planner_input.area[entity.position.x - 1][entity.position.y + 1] =
+                "reserved-for-pump"
+            planner_input.area[entity.position.x][entity.position.y - 1] =
+                "reserved-for-pump"
+            planner_input.area[entity.position.x][entity.position.y + 1] =
+                "reserved-for-pump"
+            planner_input.area[entity.position.x + 1][entity.position.y - 1] =
+                "reserved-for-pump"
+            planner_input.area[entity.position.x + 1][entity.position.y] =
+                "reserved-for-pump"
+            planner_input.area[entity.position.x + 1][entity.position.y + 1] =
+                "reserved-for-pump"
+        end
+    end
+
+    dump_planner_input(planner_input, "with-pump-reservations")
+
+    for x, reservations in pairs(planner_input.area) do
+        for y, reservation in pairs(reservations) do
+            if reservation == "undefined" then
+                if can_place_pipe(event.surface, {x = x, y = y}) then
+                    planner_input.area[x][y] = "can-build"
+                else
+                    planner_input.area[x][y] = "can-not-build"
+                end
+            end
+        end
+    end
+
+    dump_planner_input(planner_input, "with-buildable")
+
+    return planner_input
+end
+
+function dump_planner_input(planner_input, step)
+    local planner_input_as_block = serpent.block(planner_input)
+    game.write_file("pump-selection-" .. step .. ".block",
+                    planner_input_as_block)
+    local planner_input_as_json = game.table_to_json(planner_input)
+    game.write_file("pump-selection-" .. step .. ".json", planner_input_as_json)
+end
+
 function can_place_pumpjack(surface, position, direction)
     return surface.can_place_entity({
         name = "pumpjack",
         position = position,
         direction = direction,
+        force = "player",
+        build_check_type = defines.build_check_type.ghost_place
+    })
+end
+
+function can_place_pipe(surface, position)
+    return surface.can_place_entity({
+        name = "pipe",
+        position = position,
+        direction = defines.direction.north,
         force = "player",
         build_check_type = defines.build_check_type.ghost_place
     })
@@ -87,4 +193,3 @@ function get_pump_output_offset(direction)
     if direction == defines.direction.south then return {x = -1, y = 2} end
     if direction == defines.direction.west then return {x = -2, y = 1} end
 end
-
