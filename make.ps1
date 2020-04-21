5cf473dc96bdcb7da299dbe63705a70e1b6e3e6f
Koen Visser <koen@xcone.net>
2020-04-21 21:15:10 +0200

    Don't use echo-alias in ps scritp

--- make.ps1
+++ make.ps1
@@ -4,21 +4,21 @@ $stagedir = '.build\' + $mod
 $stagedirInfoJson = $stagedir + '\info.json'
 $7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
 
-echo '#Clear the build dir'
+Write-Output '#Clear the build dir'
 Remove-Item -Path .build -Force -Recurse
 
-echo '#Copy content to staging directory'
+Write-Output '#Copy content to staging directory'
 New-Item -ItemType "directory" -Path $stagedir
 Copy-Item mod\* -Destination $stagedir -Recurse
 
-echo '#Apply version'
+Write-Output '#Apply version'
 (Get-Content $stagedirInfoJson).replace('<%version%>', $version) | Set-Content $stagedirInfoJson
 
-echo '#Bundle'
+Write-Output '#Bundle'
 if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
     throw "7 zip file '$7zipPath' not found"
 }
 & $7zipPath a -tzip ('.build\' + $mod + '.zip') ('.\' + $stagedir)
 
-echo '#Clear staging directory'
+Write-Output '#Clear staging directory'
 Remove-Item -Path $stagedir -Force -Recurse
